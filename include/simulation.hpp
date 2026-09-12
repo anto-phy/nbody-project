de0018b772d9353330ea141e393b781c713f13c6
Anto <antogenovesi2510@gmail.com>
2026-09-12 23:34:56 +0200

    sim update

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -6,7 +6,7 @@
 
 namespace nbody {
 
-void update_physics(std::vector<Body>);
+void update_physics(std::vector<Body>&, Parameters);
 
 }
 
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,1 +1,21 @@
 #include "simulation.hpp"
+#include "config.hpp"
+#include <vector>
+
+namespace nbody {
+
+void update_physics(std::vector<Body>& universe, Parameters params)
+{
+  // modifica le collsion! (se a contatto li fondi)
+
+  std::vector<Body> next_universe;
+  next_universe.reserve(universe.size());
+
+  for (auto& body : universe) {
+    next_universe.push_back(body.update(universe, params));
+  }
+
+  universe = std::move(next_universe);
+}
+
+} // namespace nbody
