a12c37803e7870090c1021b9fcf3abb555cc877d
Anto <antogenovesi2510@gmail.com>
2026-09-14 16:51:07 +0200

    rendering first inp

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,6 +3,7 @@
 
 #include "body.hpp"
 #include "config.hpp"
+
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Shaders/PhongGL.h>
@@ -18,12 +19,17 @@ class Engine : public Magnum::Platform::Application
  private:
   void drawEvent() override;
 
+  // Funzione helper per il rendering del singolo corpo
+  void render_body(const Body& body, const Magnum::Matrix4& projection,
+                   const Magnum::Matrix4& camera);
+
   Magnum::GL::Mesh _sphereMesh;
-  Magnum::Shaders::PhongGL _pbrShader;
+  Magnum::Shaders::PhongGL _shader; // Usa PhongGL
   std::vector<Body> _bodies;
   Parameters _params;
-  double _visualScale{1.0e-9}; // Scales physical meters to rendering units
+  double _visualScale{1.0e-9};
 };
 
 } // namespace nbody
+
 #endif
